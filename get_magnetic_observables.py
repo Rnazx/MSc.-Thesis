e2284b12288a5007c5032026bea76574ccc64597
Rion <rionazareth@gmail.com>
2023-04-16 13:26:46 +0530

    made appropriate changes

--- get_magnetic_observables.py
+++ get_magnetic_observables.py
@@ -1,56 +1,36 @@
+from helper_functions import datamaker, root_finder, exp_analytical_data
 import numpy as np
-import matplotlib.pyplot as plt
 from sympy import *
-from fractions import Fraction
 import pickle
 import os
-from matplotlib.ticker import FormatStrFormatter
-import sys
+
 import subprocess
 
 current_directory = str(os.getcwd())
 
-#conversion factors
-pc_kpc = 1e3		#number of pc in one kpc
-cm_km = 1e5		#number of cm in one km
-s_day = 24*3600		#number of seconds in one day
-s_min = 60		#number of seconds in one hour
-s_hr = 3600		#number of seconds in one hour
-cm_Rsun = 6.957e10	#solar radius in cm
-g_Msun = 1.989e33	#solar mass in g
-cgs_G = 6.674e-8 
-cms_c = 2.998e10
-g_mH = 1.6736e-24
-g_me = 9.10938e-28
-cgs_h = 6.626e-27
-deg_rad = 180e0/np.pi
-arcmin_deg = 60e0
-arcsec_deg = 3600e0
-kpc_cm = 3.086e+21  #number of ccentimeters in one parsec
-Myr_s = 1e+6*(365*24*60*60) #megayears to seconds
-
-#data extraction for galaxy
+# data extraction for galaxy
 subprocess.run(["python", "zipped_data.py"])
 
-os.chdir(current_directory +'\data')
+os.chdir(current_directory + '\data')
 
 with open('zip_data.pickle', 'rb') as f:
-     data_pass = pickle.load(f)
+    data_pass = pickle.load(f)
 
-#extracting the expressions
+# extracting the expressions
 os.chdir(current_directory + '\expressions')
-subprocess.run(["python", "turbulence_expressions.py"])
-subprocess.run(["python", "magnetic_expressions.py"])
+# subprocess.run(["python", "turbulence_expressions.py"])
+# subprocess.run(["python", "magnetic_expressions.py"])
 
 
 with open('turb_exp.pickle', 'rb') as f:
-     hg, rho, nu, u, l, taue, taur, alphak1, alphak2, alphak3 = pickle.load(f)
+    hg, rho, nu, u, l, taue, taur, alphak1, alphak2, alphak3 = pickle.load(f)
 
 with open('mag_exp.pickle', 'rb') as f:
-     biso, bani, Bbar, tanpb, tanpB, Beq, eta = pickle.load(f)
+    biso, bani, Bbar, tanpb, tanpB, Beq, eta = pickle.load(f)
 
 os.chdir(current_directory)
-from helper_functions import datamaker, root_finder, exp_analytical_data
+
+
 
 h_f = root_finder(exp_analytical_data(hg, data_pass))
 
@@ -58,11 +38,7 @@ l_f = datamaker(l, data_pass, h_f)
 u_f = datamaker(u, data_pass, h_f)
 taue_f = datamaker(taue, data_pass, h_f)
 taur_f = datamaker(taur, data_pass, h_f)
-tau_f = taue_f
-
-for i in range(len(tau_f)):
-     if taue_f[i]<taur_f[i]: tau_f[i] = taue_f[i]
-     else: tau_f[i] = taur_f[i]
+tau_f = np.minimum(taue_f, taur_f)
 
 omega = Symbol('\Omega')
 kalpha = Symbol('K_alpha')
@@ -74,17 +50,16 @@ kah = datamaker(kalpha/calpha, data_pass, h_f, tau_f)*(h_f/(tau_f*u_f))
 alphak_f = []
 
 for i in range(len(omt)):
-     if min(1, kah[i]) >= omt[i]:
-          alpha_k = alphak1         
-     elif min(omt[i], kah[i]) >= 1:
-          alpha_k = alphak2
-     else:
-          alpha_k = alphak3
-     alphak_f.append(datamaker(alpha_k, [data_pass[i]], np.array([h_f[i]]), np.array([tau_f[i]]))[0])
+    if min(1, kah[i]) >= omt[i]:
+        alpha_k = alphak1
+    elif min(omt[i], kah[i]) >= 1:
+        alpha_k = alphak2
+    else:
+        alpha_k = alphak3
+    alphak_f.append(datamaker(alpha_k, [data_pass[i]], np.array(
+        [h_f[i]]), np.array([tau_f[i]]))[0])
 
 alphak_f = np.array(alphak_f)
-
-
 
 
 biso_f = datamaker(biso, data_pass, h_f, tau_f)
--- helper_functions.py
+++ helper_functions.py
@@ -3,24 +3,6 @@ from sympy import *
 import inspect
 from scipy.optimize import curve_fit, fsolve
 
-###############################################################################################
-pc_kpc = 1e3		#number of pc in one kpc
-cm_km = 1e5		#number of cm in one km
-s_day = 24*3600		#number of seconds in one day
-s_min = 60		#number of seconds in one hour
-s_hr = 3600		#number of seconds in one hour
-cm_Rsun = 6.957e10	#solar radius in cm
-g_Msun = 1.989e33	#solar mass in g
-cgs_G = 6.674e-8 
-cms_c = 2.998e10
-g_mH = 1.6736e-24
-g_me = 9.10938e-28
-cgs_h = 6.626e-27
-deg_rad = 180e0/np.pi
-arcmin_deg = 60e0
-arcsec_deg = 3600e0
-kpc_cm = 3.086e+21  #number of ccentimeters in one parsec
-Myr_s = 1e+6*(365*24*60*60) #megayears to seconds
 
 ############################################################################################################################
 # Defining the Observables
@@ -52,6 +34,8 @@ psi = Symbol('psi')
 kalpha = Symbol('K_alpha')
 bet = Symbol('beta')
 alphak = Symbol('alpha_k')
+Gamma = Symbol('Gamma')
+
 
 # Defining the general parameters
 u = Symbol('u')
@@ -61,51 +45,70 @@ h = Symbol('h')
 
 cs = (gamma*boltz*T/(mu*mh))**Rational(1/2)
 
+###############################################################################################
+
+g_Msun = 1.989e33  # solar mass in g
+cgs_G = 6.674e-8  # gravitational constant in cgs units
+g_mH = 1.6736e-24  # mass of hydrogen atom in grams
+cgs_kB = 1.3807e-16  # boltzmann constant in cgs units
+
+gval, clval, xioval, mstarval, deltaval, e51val, kaval, Gammaval = tuple(
+    np.genfromtxt('constants.in', delimiter='=', dtype=np.float64)[:, -1])
+
+const = [(boltz, cgs_kB), (mh, g_mH), (G, cgs_G), (gamma, gval),
+         (cl, clval), (xio, xioval), (mstar, mstarval*g_Msun), (delta, deltaval), (E51, e51val), (kalpha, kaval), (Gamma, Gammaval)]
+
+######################################################################################################################
+
 
 def power_law(x, a, b):
     return a*np.power(x, b)
+
 
 def list_transpose(x):
     array = np.array(x)
     transposed_array = array.T
     return transposed_array.tolist()
 ######################################################################################################################
+
+
 def retrieve_name(var):
     callers_local_vars = inspect.currentframe().f_back.f_locals.items()
     return [var_name for var_name, var_val in callers_local_vars if var_val is var][-1]
 
 ###############################################################################################################
 
+
 def exp_analytical_data(express, data_pass):
+    express = express.subs(const).simplify(force=True)
 
-    const = [(gamma, 1.5), (boltz, 1.3807e-16), (mh, 1.67e-24), (mach, 1), (G, 6.67e-8),
-            (cl, 3/4), (xio, 0.4), (mstar, 0.85*g_Msun), (delta, 8e-3), (E51, 1), (kalpha, 1)]
-  
-    express = express.subs(const).simplify(force=True) 
+    exp = np.array([express.evalf(subs={sigmatot: sigt, sigma: sig, sigmasfr: sigsfr, q: qs, omega: oms, zet: zets, T: t,
+                   psi: ps, bet: b, calpha: ca, Rk: rk, mu: m}) for sigt, sig, sigsfr, qs, oms, zets, t, ps, b, ca, rk, m in data_pass])
 
-    exp = np.array([express.evalf(subs={ sigmatot:sigt, sigma: sig, sigmasfr: sigsfr, q:qs, omega: oms, zet:zets, T:t, psi:ps, bet :b, calpha: ca, Rk: rk, mu: m}) for sigt,sig, sigsfr,qs, oms, zets, t, ps, b, ca, rk, m in data_pass])
-    
     return exp
 
 ############################################################################################################################
 
-def datamaker(quan, data_pass, h_f, tau_f = None, alphak_f = None):
+
+def datamaker(quan, data_pass, h_f, tau_f=None, alphak_f=None):
     quan_val = exp_analytical_data(quan, data_pass)
-    if tau_f is None: 
+    if tau_f is None:
         tau_f = np.ones(len(h_f))
     if alphak_f is None:
-        return  np.array([np.float64( quan_val[i].evalf(subs= {h : hf, tau : tauf} )) for i, (hf, tauf) in enumerate(zip(h_f, tau_f))])
-    else: 
-        Bbar_in = np.array([quan_val[i].evalf(subs= {h : hf, tau : tauf, alphak : alphakf} ) for i, (hf, tauf, alphakf) in enumerate(zip(h_f, tau_f, alphak_f))])
-        return np.float64(Bbar_in*(np.float64(Bbar_in*Bbar_in>0)))
-        
-    
+        return np.array([np.float64(quan_val[i].evalf(subs={h: hf, tau: tauf})) for i, (hf, tauf) in enumerate(zip(h_f, tau_f))])
+    else:
+        Bbar_in = np.array([quan_val[i].evalf(subs={h: hf, tau: tauf, alphak: alphakf}) for i, (
+            hf, tauf, alphakf) in enumerate(zip(h_f, tau_f, alphak_f))])
+        return np.float64(Bbar_in*(np.float64(Bbar_in*Bbar_in > 0)))
+
+
 ##############################################################################################################################################
 
-def root_finder(h_val, h_init = 7e+25):
+def root_finder(h_val, h_init=7e+25):
     h_f = []
     for hv in h_val:
-        func = lambda x : np.array([np.float64((h-hv).evalf(subs={h : i})) for i in x])
+        def func(x): return np.array(
+            [np.float64((h-hv).evalf(subs={h: i})) for i in x])
         h_solution = fsolve(func, h_init)
         h_f.append(h_solution[0])
     h_f = np.array(h_f)
@@ -114,43 +117,44 @@ def root_finder(h_val, h_init = 7e+25):
 
 #####################################################################################################
 
-def scal_helper(express, data_pass, observable = zet, _range = np.linspace(1,5000,50)):
 
-    const = [(gamma, 1.5), (boltz, 1.3807e-16), (mh, 1.67e-24), (mach, 1), (G, 6.67e-8),
-            (cl, 3/4), (xio, 0.4), (mstar, 0.85*g_Msun), (delta, 8e-3), (E51, 1), (kalpha, 1)]
-  
+def scal_helper(express, data_pass, observable=zet, _range=np.linspace(1, 5000, 50)):
     express = express.subs(const).simplify(force=True)
-    sigt,sig, sigsfr,qs, oms, zets, t, ps, b, ca, rk, m = data_pass[0]
-    val_subs = { sigmatot:sigt, sigma: sig, sigmasfr: sigsfr, q:qs, omega: oms, zet:zets, T:t, psi:ps, bet :b, calpha: ca, Rk: rk, mu: m}
+    sigt, sig, sigsfr, qs, oms, zets, t, ps, b, ca, rk, m = data_pass[0]
+    val_subs = {sigmatot: sigt, sigma: sig, sigmasfr: sigsfr, q: qs,
+                omega: oms, zet: zets, T: t, psi: ps, bet: b, calpha: ca, Rk: rk, mu: m}
     try:
-        #val_subs.pop(observable)
-        obs_val = (val_subs.pop(observable))*_range
+        val_subs.pop(observable)
+        obs_val = _range#(val_subs.pop(observable))*_range
     except ValueError:
         print('Observable does not exist!')
-    exp = express.evalf(subs = val_subs)
-    return obs_val, np.array([exp.evalf(subs = {observable:o}) for o in obs_val])
+    exp = express.evalf(subs=val_subs)
+    return obs_val, np.array([exp.evalf(subs={observable: o}) for o in obs_val])
 
 
-def scal_finder(h_exp, quan_exp, observable, data_pass, tau_exp = None, alpha_exp = None, _range = np.linspace(1,5000,200)):
-    def scal_dat(quan, data_pass, h_f, tau_f = None, alphak_f = None):
+def scal_finder(h_exp, quan_exp, observable, data_pass, tau_exp=None, alpha_exp=None, _range=np.linspace(1, 5000, 200)):
+    def scal_dat(quan, data_pass, h_f, tau_f=None, alphak_f=None):
         quan_val = scal_helper(quan, data_pass, observable, _range)[1]
-        if tau_f is None: 
+        if tau_f is None:
             tau_f = np.ones(len(h_f))
         if alphak_f is None:
-            return  np.array([np.float64( quan_val[i].evalf(subs= {h : hf, tau : tauf} )) for i, (hf, tauf) in enumerate(zip(h_f, tau_f))])
-        else: 
-            Bbar_in = np.array([quan_val[i].evalf(subs= {h : hf, tau : tauf, alphak : alphakf} ) for i, (hf, tauf, alphakf) in enumerate(zip(h_f, tau_f, alphak_f))])
-            return np.float64(Bbar_in*(np.float64(Bbar_in*Bbar_in>0)))
+            return np.array([np.float64(quan_val[i].evalf(subs={h: hf, tau: tauf})) for i, (hf, tauf) in enumerate(zip(h_f, tau_f))])
+        else:
+            Bbar_in = np.array([quan_val[i].evalf(subs={h: hf, tau: tauf, alphak: alphakf}) for i, (
+                hf, tauf, alphakf) in enumerate(zip(h_f, tau_f, alphak_f))])
+            return np.float64(Bbar_in*(np.float64(Bbar_in*Bbar_in > 0)))
     obs_val, h_val = scal_helper(h_exp, data_pass, observable, _range)
-    h_scal = root_finder(h_val, 7e+25)
-    if tau_exp is not None : tau_scal = scal_dat(tau_exp, data_pass, h_scal)
-    else : tau_scal = None
-
-    if alpha_exp is not None : alphak_scal = scal_dat(alpha_exp, data_pass, h_scal, tau_scal)
-    else : alphak_scal = None
-
+    h_scal = root_finder(h_val, 7e+20)
+    if tau_exp is not None:
+        tau_scal = scal_dat(tau_exp, data_pass, h_scal)
+    else:
+        tau_scal = None
+    if alpha_exp is not None:
+        alphak_scal = scal_dat(alpha_exp, data_pass, h_scal, tau_scal)
+    else:
+        alphak_scal = None
     quan_f = scal_dat(quan_exp, data_pass, h_scal, tau_scal, alphak_scal)
-
-    #pg, cov = curve_fit(f=power_law, xdata=obs_val, ydata=quan_f, p0=[0, 0], bounds=(-np.inf, np.inf))
-    pg = np.mean(obs_val*(np.gradient(np.log(np.abs(quan_f)))/np.gradient(obs_val)))
+    # pg, cov = curve_fit(f=power_law, xdata=obs_val, ydata=quan_f, p0=[0, 0], bounds=(-np.inf, np.inf))
+    pg = np.mean(
+        obs_val*(np.gradient(np.log(np.abs(quan_f)))/np.gradient(obs_val)))
     return obs_val, quan_f, pg
