192b1c75aae344824c020a417a1cb7199faab690
Your Name <you@example.com>
2023-07-11 18:25:19 +0530

    edits

--- zipped_data.py
+++ zipped_data.py
@@ -4,12 +4,13 @@ import pickle
 import os
 import subprocess
 
-current_directory = str(os.getcwd())  # store the path of the current directory
+current_directory = r'S:\MSc.-Thesis-master' # store the path of the current directory
 
 params = {}
 switch = {}
 
-with open('parameter_file.in', 'r') as FH:
+#opening these files and making them into dictionaries
+with open(current_directory+'\parameter_file.in', 'r') as FH:
     for file in FH.readlines():
         line = file.strip()
         try:
@@ -20,11 +21,11 @@ with open('parameter_file.in', 'r') as FH:
                 "Failed while unpacking. Not enough arguments to supply.")
         try:
             params[par_name] = np.float64(value)
-        except ValueError:
+        except ValueError: #required cz of 14/11 in parameter.in file
             num, denom = value.split('/')
             params[par_name] = np.float64(num) / np.float64(denom)
 
-with open('switches.in', 'r') as FH:
+with open(current_directory+'\switches.in', 'r') as FH:
     for file in FH.readlines():
         line = file.strip()
         try:
@@ -40,14 +41,14 @@ print('Succesfully read the parameters and switches')
 
 # data extraction for galaxy
 os.chdir(current_directory + '\data')
-if switch['chem_or_claude'] is 'Chemin':
-    c_or_cl = 1
+if switch['chem_or_claude'] == 'Chemin':
+    c_or_cl = 1 #these values are chosen in switches.in file
 else:
     c_or_cl = 0
 
-subprocess.run(["python", "data_conv_M31.py", str(int(c_or_cl))])
-
-with open('data_m31.pickle', 'rb') as f:
+#choose "data_conv_M33.py" or "data_conv_M31.py" in the next two lines to choose the galaxy
+subprocess.run(["python", "data_conv_M33.py", str(int(c_or_cl))])
+with open('data_m33.pickle', 'rb') as f:
     data = pickle.load(f)
 
 kpc_r, dat_sigmatot, dat_sigmaHI, dat_q, dat_omega, dat_sigmasfr, molfrac = data
@@ -58,7 +59,7 @@ dat_sigmaH2 = dat_sigmaHI*(1/(1-molfrac))
 
 T_tb = (0.017*kpc_r + 0.5)*1e+4
 
-if switch['incl_moldat'] is 'Yes':
+if switch['incl_moldat'] == 'Yes':
     dat_sigma = dat_sigmaHI + dat_sigmaH2
 else:
     dat_sigma = dat_sigmaHI
@@ -69,15 +70,15 @@ ks_exp = params['ks_exp']
 ks_const = (dat_sigmasfr/(dat_sigma)**(ks_exp)).mean()
 
 ks_split = switch['force_kennicut_scmidt'].split()
-if ks_split[0] is 'Yes':
-    if ks_split[1] is 'sigmasfrdata':
+if ks_split[0] == 'Yes':
+    if ks_split[1] == 'sigmasfrdata':
         dat_sigma = (dat_sigmasfr/ks_const)**(1/ks_exp)
     else:
         dat_sigmasfr = ks_const*(dat_sigma)**(ks_exp)
 
 os.chdir(current_directory)
 
-
+#changes based on galaxy and quality of fit
 zet = params['\zeta']*np.ones(r)
 psi = params['\psi']*np.ones(r)
 bet = params[r'\beta']*np.ones(r)
@@ -86,6 +87,7 @@ T = T_tb  # 1e+4*np.ones(r)
 ca = params[r'C_\alpha']*np.ones(r)
 rk = params['R_\kappa']*np.ones(r)
 mu = params['mu']*np.ones(r)
+# zip function makes array of corresponding elements of each array passed into it
 data_pass = kpc_r, list(zip(dat_sigmatot, dat_sigma, dat_sigmasfr,
                  dat_q, dat_omega, zet, T, psi, bet, ca, rk, mu))
 
