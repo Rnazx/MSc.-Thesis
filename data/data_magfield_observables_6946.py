5ce206116ec705d1fbef17d8915a7e84b7e28723
Your Name <you@example.com>
2023-07-24 00:49:53 +0530

    6946

--- data/data_magfield_observables_6946.py
+++ data/data_magfield_observables_6946.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-#no po data for ngc 6946
+#no pb data for ngc 6946
 
 # Magnetic field data from Beck et. al. 2019 Tables 3 and 4
 G_dat_Bord = np.array([5.1,5.1])
@@ -8,10 +8,14 @@ G_dat_Breg = np.array([19*0.06,13*0.15])
 G_dat_Btot = np.array([19,13])
 
 #pitch angle data #RM= rotation measure data, M= fitting polarisation angles
-RM_dat_po = np.array([27,21,10,30,32,10]) * np.pi/180 #pitch angle of ordered field
-err_RM_dat_po = np.array([2,2,6,2,4,5]) * np.pi/180 #error in po
-rmdat_tanpo = np.tan(RM_dat_po)
-rm_errdat_tanpo = 1/(np.cos(err_RM_dat_po))**2
+RM_dat_po_range1 = np.array([27,21,10]) * np.pi/180 #pitch angle of ordered field
+RM_dat_po_range2 = np.array([30,32,10]) * np.pi/180 
+err_RM_dat_po_range1 = np.array([2,2,6]) * np.pi/180 #error in po
+err_RM_dat_po_range2 = np.array([2,4,5]) * np.pi/180 
+rmdat_tanpo_range1 = np.tan(RM_dat_po_range1)
+rmdat_tanpo_range2 = np.tan(RM_dat_po_range2)
+rm_errdat_tanpo_range1 = 1/(np.cos(err_RM_dat_po_range1))**2
+rm_errdat_tanpo_range2 = 1/(np.cos(err_RM_dat_po_range2))**2
 
 # M_dat_pb = np.array(['nan',20,24,22,18,'nan']) * np.pi/180 #pitch angle of regular field
 # err_M_dat_pb = np.array(['nan',1,4,4,1,'nan']) * np.pi/180
@@ -22,6 +26,8 @@ rm_errdat_tanpo = 1/(np.cos(err_RM_dat_po))**2
 # m_errdat_tanpb = 1/(np.cos(err_M_dat_pb))**2
 
 #radial ranges #discontinuous ranges in Beck+19
+range1=[0,6,12,18]
+range2=[1.5,5.5,8.5]
 # mrange_endps = np.array([]) #lower limit of radial ranges where M is used, given in table 4, Beck+19
 # mrange = (mrange_endps[1:] + mrange_endps[:-1])/2 #average of each of the intervals given in above array. contains 1 less point than above array
 #rmrange = np.arange(7.5, 12, 1) #radial ranges where RM is used, given in table 4, Beck+19
